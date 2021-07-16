3575496a95496676132caa1f8f0df0b923ffa586
bukharov <dilmurod.bukharov@gmail.com>
2021-07-16 16:04:39 +0500

    made some changes in html and css files

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+const url = "https://api.fisenko.net/quotes?l=en";
+header("Access-Control-Allow-Origin: https://api.fisenko.net/quotes?l=en");
+fetch(url)
+  .then((res) => {
+    console.log("success!", res);
+  })
+  .catch((err) => {
+    console.log("Something went wrong...", err);
+  });
+
 console.log("connected");
+document.getElementById("quote").innerHTML = "Hello new text is here";
