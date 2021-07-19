579b27de210481cf0e084ee49e62d049dc97a59f
bukharov <dilmurod.bukharov@gmail.com>
2021-07-19 15:21:53 +0500

    created second api fetch, managed to display it

--- main.js
+++ main.js
@@ -16,3 +16,30 @@ function getAdvice() {
     });
   document.getElementById("button-text").innerText = "Next advice";
 }
+
+const url1 =
+  "https://opentdb.com/api.php?amount=15&category=23&difficulty=medium&type=boolean";
+var allResults = {};
+function playTrivia() {
+  fetch(url1)
+    .then((res) => {
+      return res.json();
+    })
+    .then((res) => {
+      console.log("Success trivia", res);
+      allResults = res;
+    })
+    .catch((err) => {
+      console.log("something went wrong...", err);
+    });
+}
+let count = 0;
+function nextQuestion() {
+  if (count < 15) {
+    document.getElementById("quote").innerHTML =
+      allResults.results[count].question;
+  } else {
+    document.getElementById("quote").innerHTML = "End of the questions";
+  }
+  count += 1;
+}
